ba880ca8872a0347717c1299cfd8616ffb882173
alya <alya-892@yandex.ru>
2022-11-03 23:46:46 +0300

    задача 2

---
+++ EXAMPLE2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число"); 
+int N=  Convert.ToInt32 (Console.ReadLine()); 
+int a=1; 
+while (a<=N) 
+{ 
+    double S=Math.Pow(a,3); 
+    a++; 
+    Console.WriteLine ( S );}
