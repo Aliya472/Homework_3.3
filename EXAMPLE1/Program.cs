0b8c56dd8ef42a0f4fc11613764bf7dca5c1d64b
alya <alya-892@yandex.ru>
2022-11-03 23:22:33 +0300

    задача 1

---
+++ EXAMPLE1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите пятизначное число"); 
+int a=  Convert.ToInt32 (Console.ReadLine()); 
+string n=a.ToString(); 
+if(n[0] == n[4])  
+{ 
+   if(n[1] == n[3])  
+{ 
+    Console.WriteLine("Да"); 
+} 
+ else Console.WriteLine("Нет"); 
+} 
+ else  
+ Console.WriteLine("Нет");
